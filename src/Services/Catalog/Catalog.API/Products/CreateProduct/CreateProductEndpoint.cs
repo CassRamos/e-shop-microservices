3d0ac4bc5bf15d5feb393d684a7d343ec0ad19f6
Cass <cassianoleaoramos@gmail.com>
2025-06-29 11:41:22 -0300

    Implement CreateProduct endpoint using CQRS pattern

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -9,7 +9,25 @@
 
     public record CreateProductResponse(Guid Id);
 
-    public class CreateProductEndpoint
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithTags("Create Product")
+                .WithDescription("Create Product");
+        }
     }
 }
