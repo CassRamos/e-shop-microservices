b28b6ae3e478b434bc005d028b64650ed4d79c66
Cass <cassianoleaoramos@gmail.com>
2025-07-03 19:18:06 -0300

    Add GetProductByIdHandler and GetProductByIdEndpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetPorductByIdResponse(Product product);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+
+                var response = result.Adapt<GetPorductByIdResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetProductById")
+                .Produces<GetPorductByIdResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product By Id")
+                .WithDescription("Get a product by its unique identifier.");
+        }
+    }
+}
